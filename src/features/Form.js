f6d73186a4ca30ea021295d07316dd9207edb305
Travik Vlad <travikdev@gmail.com>
2023-06-02 00:12:41 +0100

    Register form is ready

---
+++ src/features/Form.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Form = () => {
+  return (
+    <section>
+      <form>
+        <div>
+          <label htmlFor='username'>Username: </label>
+          <input 
+            type='text'
+            id='username'
+             
+          />
+        </div>
+      </form>
+    </section>
+  )
+}
