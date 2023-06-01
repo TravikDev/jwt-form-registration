7abbe09f069df16786c5412326644015bac12552
Travik Vlad <travikdev@gmail.com>
2023-06-02 00:13:17 +0100

    Register form is ready for requests

---
+++ src/Register.js
@@ -0,0 +1,201 @@
+import { useState, useRef, useEffect } from "react"
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,24}$/
+
+export const Register = () => {
+
+  const userRef = useRef()
+  const errRef = useRef()
+
+  const [user, setUser] = useState('')
+  const [userValid, setUserValid] = useState(false)
+  const [userFocus, setUserFocus] = useState(false)
+
+  const [pwd, setPwd] = useState('')
+  const [pwdValid, setPwdValid] = useState(false)
+  const [pwdFocus, setPwdFocus] = useState(false)
+
+  const [matchPwd, setMatchPwd] = useState('')
+  const [matchPwdValid, setMatchPwdValid] = useState(false)
+  const [matchPwdFocus, setMatchPwdFocus] = useState(false)
+
+  const [errMsg, setErrMsg] = useState('')
+  const [success, setSuccess] = useState('')
+
+  useEffect(() => {
+    userRef.current.focus()
+  }, [])
+
+  useEffect(() => {
+    const result = USER_REGEX.test(user)
+    console.log(result)
+    console.log(user)
+    setUserValid(result)
+  }, [user])
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(pwd)
+    console.log(result)
+    console.log(pwd)
+    setPwdValid(result)
+    const match = pwd === matchPwd
+    setMatchPwdValid(match)
+  }, [pwd, matchPwd])
+
+  useEffect(() => {
+    setErrMsg('')
+  }, [user, pwd, matchPwd])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const v1 = USER_REGEX.test(user)
+    const v2 = PWD_REGEX.test(pwd)
+    if (!v1 || !v2) {
+      setErrMsg('Invalid Entry')
+      return
+    }
+    console.log(user, pwd)
+    setSuccess(true)
+  }
+
+  return (
+    <>
+      {success
+        ? (
+          <p>Success!</p>)
+        : (
+          < section >
+            <p ref={errRef} style={errMsg ? style.errmsg : style.offscreen}>{errMsg}</p>
+            <h1> Register </h1>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="username">
+                Username:
+                <span style={userValid ? style.valid : style.hide}>
+                  <FontAwesomeIcon icon={faCheck} />
+                </span>
+                <span style={userValid || !user ? style.hide : style.unvalid}>
+                  <FontAwesomeIcon icon={faTimes} />
+                </span>
+              </label>
+              <input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+                aria-invalid={userValid ? "false" : "true"}
+                aria-describedby="uidnote"
+                onFocus={() => setUserFocus(true)}
+                onBlur={() => setUserFocus(false)}
+              />
+              <p id='uidnote' style={userFocus && user && !userValid ? style.note : style.offscreen}>
+                <FontAwesomeIcon icon={faInfoCircle} />
+                4 to 24 characters.<br />
+                Must begin with a letter.<br />
+                Letters, numbers, underscores, hyphens allowed.
+              </p>
+              <br />
+              <label htmlFor="password">
+                Password:
+                <span style={pwdValid ? style.valid : style.hide}>
+                  <FontAwesomeIcon icon={faCheck} />
+                </span>
+                <span style={pwdValid || !pwd ? style.hide : style.unvalid}>
+                  <FontAwesomeIcon icon={faTimes} />
+                </span>
+              </label>
+              <input
+                type="password"
+                id="password"
+                autoComplete="off"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+                aria-invalid={pwdValid ? "false" : "true"}
+                aria-describedby="pwdnote"
+                onFocus={() => setPwdFocus(true)}
+                onBlur={() => setPwdFocus(false)}
+              />
+              <p id='pwdnote' style={pwdFocus && pwd && !pwdValid ? style.note : style.offscreen}>
+                <FontAwesomeIcon icon={faInfoCircle} />
+                8 to 24 characters.<br />
+                Must include uppercase and lowercase letters, a number and a special character.<br />
+                Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+              </p>
+              <br />
+              <label htmlFor="matchPwd">
+                Confirm password:
+                <span style={matchPwdValid && matchPwd ? style.valid : style.hide}>
+                  <FontAwesomeIcon icon={faCheck} />
+                </span>
+                <span style={matchPwdValid || !matchPwd ? style.hide : style.unvalid}>
+                  <FontAwesomeIcon icon={faTimes} />
+                </span>
+              </label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setMatchPwd(e.target.value)}
+                value={matchPwd}
+                required
+                aria-invalid={matchPwdValid ? "false" : "true"}
+                aria-describedby="matchpwdnote"
+                onFocus={() => setMatchPwdFocus(true)}
+                onBlur={() => setMatchPwdFocus(false)}
+              />
+              <p id='matchpwdnote' style={matchPwdFocus && matchPwd && !matchPwdValid ? style.note : style.offscreen}>
+                <FontAwesomeIcon icon={faInfoCircle} />
+                Must match the first password input field.
+              </p>
+              <br />
+              <button disabled={!userValid || !pwdValid || !matchPwdValid ? true : false}>Sign up</button>
+
+              <p>
+                Already registered? <br />
+                <span>
+                  <a href="#">Log in</a>
+                </span>
+              </p>
+
+            </form>
+          </section >
+        )}
+    </>
+  )
+}
+
+const style = {
+  errmsg: {
+    background: '#fff',
+    borderRadius: 1,
+    borderColor: '#000',
+    display: 1
+  },
+  note: {
+    background: '#f44343',
+    borderRadius: 1,
+    textColor: '#fff',
+    borderColor: '#000',
+  },
+  offscreen: {
+    background: '#f44343',
+    borderRadius: 1,
+    textColor: '#fff',
+    borderColor: '#000',
+    display: 'none'
+  },
+  valid: {
+    color: '#0f0'
+  },
+  unvalid: {
+    color: '#f00'
+  },
+  hide: {
+    display: 'none'
+  }
+}
---
+++ todo.md
@@ -0,0 +1,18 @@
+<!-- General -->
+# 1. Create the login form component
+# 2. Import icons
+# 3. Create regexp for username and pswd
+# 4. Preparing useRef hooks for user and err msgs
+
+# 5. Added states: user string and booleans for validation and focus
+# 6. Added states: pswd string and booleans for validation and focus
+# 7. Added states: pswd match string and booleans for validation and focus
+
+# 8. useEffect: firstly focus on username field
+# 9. useEffect: (1) check username regexp (2) set boolean result of checked username
+# 10. UseEffect: (1) check pwd regexp (2) set boolean result of checked pwd (3) check match pwd with matchPwd (4) set boolean result of checked matches
+
+# 11. Added area for error message with dynamic css
+# 12. Added inputs with tips areas and validation: user, password, matchPassword and submit button
+
+# 13. Added form submit async function with validation inside 
